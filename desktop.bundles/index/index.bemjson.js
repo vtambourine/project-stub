ceaf57392b30d6f1478fc2fb608ab7494eceb551
Benjamin Tambourine <vtambourine@yandex.ru>
2012-12-15 01:07:39 +0400

    Hello block on main page

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -12,22 +12,7 @@
     ],
     content:[
         {
-            block: 'header',
-            content: [
-                'header content goes here'
-            ]
-        },
-        {
-            block: 'content',
-            content: [
-                'main content'
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                'footer content goes here'
-            ]
+            block: 'hello'
         }
     ]
 })
